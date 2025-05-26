f0d96f2b046cdb493326b7fc471bfd656fbc6cc4
Alro John <alromercado08@gmail.com>
2025-05-26 20:19:15 +0800

    push now

---
+++ src/app/(dashboard)/equipments/page.tsx
@@ -0,0 +1,13 @@
+'use client';
+
+
+
+const AllEquipmentsPage = () => {
+    return (
+        <div>
+            <h1>AllEquipmentsPage</h1>
+        </div>
+    );
+};
+
+export default AllEquipmentsPage;
---
+++ src/app/(dashboard)/vehicles/page.tsx
@@ -0,0 +1,11 @@
+"use client";
+
+const AllVehiclesPage = () => {
+  return (
+    <div>
+      <h1>AllVehiclesPage</h1>
+    </div>
+  );
+};
+
+export default AllVehiclesPage;
