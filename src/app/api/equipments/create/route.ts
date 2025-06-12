663e2a13f1d91ccd89a3ce2ea6f7a8793b532319
Alro John <alromercado08@gmail.com>
2025-06-12 12:35:59 +0800

    push now

--- src/app/api/equipments/create/route.ts
+++ src/app/api/equipments/create/route.ts
@@ -47,7 +47,6 @@ export async function POST(request: NextRequest) {
                 brand,
                 model,
                 type,
-                expirationDate: new Date(expirationDate),
                 status: status || 'OPERATIONAL',
                 remarks,
                 owner,
